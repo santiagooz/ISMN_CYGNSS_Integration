86d08cb9696db8a32b35da26fb47c3c81704bede
Santiago Ozafrain <sozafrain@gmail.com>
2024-01-31 15:42:52 -0300

    Changed the format of the CYGNSS data downloaded from openDAP to netCDF4 instead of csv

--- retrieve_CYGNSS_data.py
+++ retrieve_CYGNSS_data.py
@@ -1,4 +1,3 @@
-import re
 import datetime
 import requests
 import json
@@ -6,11 +5,51 @@ from requests.auth import HTTPBasicAuth
 from requests.exceptions import ChunkedEncodingError
 import numpy as np
 import time
+import os
+import netCDF4 as nc
+from netCDF4 import Dataset
+from scipy.interpolate import interp1d
+
+# Function to read credentials from a file
+def read_credentials(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        username = lines[0].strip()
+        password = lines[1].strip()
+    return username, password
+
+# Function to calculate the width of DDM waveforms
+def WF_width(DDM, K):
+    peak_width = []
+    for jj in range(DDM.shape[0]):
+        ddm0 = DDM[jj, :, :]
+        max_row_index = np.argmax(ddm0, axis=1)
+        max_row_values = ddm0[range(ddm0.shape[0]), max_row_index]
+        interp_function = interp1d(range(ddm0.shape[0]), max_row_values, kind='cubic')
+        interp_points = np.linspace(0, ddm0.shape[0] - 1, K)
+        interp_curve = interp_function(interp_points)
+        max_value = np.max(interp_curve)
+        threshold_value = 0.7 * max_value
+        indices_below_threshold = np.where(interp_curve >= threshold_value)[0]
+
+        # Calculate width
+        if len(indices_below_threshold) >= 2:
+            peak_width.append(interp_points[indices_below_threshold[-1]] - interp_points[indices_below_threshold[0]])
+        else:
+            peak_width.append(-1)
+    return peak_width
 
 # Function to update input data with CYGNSS information
-def update_input_data(input_data, CYGNSS_info, close_points, dist_to_station, input_vars, date):
+def update_input_data(input_data, CYGNSS_info, close_points, dist_to_station, input_vars, date, ddmID):
     for var in input_vars:
-        input_data[var].extend(CYGNSS_info[var][close_points])
+        if var == 'brcs':
+            ddm = CYGNSS_info[var][close_points, ddmID-1, :, :]
+            input_data[var].extend([float(value) for value in WF_width(ddm, 100)])
+        elif var == 'ddm_timestamp_utc':
+            input_data[var].extend(CYGNSS_info[var][close_points])
+        else:
+            input_data[var].extend([float(value) for value in CYGNSS_info[var][close_points, ddmID-1]])
+
     input_data['date'].extend([date] * len(close_points))
     input_data['dist_to_station'].extend(dist_to_station[close_points])
 
@@ -25,34 +64,29 @@ def haversine(lat1, lon1, lat2, lon2):
     distance = radius * c
     return distance
 
-# Function to extract values from CSV content
-def extract_values(csv_content, var_list, offset):
-    CYGNSS_info = {'status': False}
-    start_idx = {}
-
-    # Find starting indices for each variable in the CSV content
-    for key in var_list:
-        start_idx[key] = csv_content.find(key)
-
-    # Sort indices based on their positions in the content
-    sorted_start_idx = {k: v for k, v in sorted(start_idx.items(), key=lambda item: item[1])}
-    start_idx_values = list(sorted_start_idx.values())
-    start_idx_keys = list(sorted_start_idx.keys())
-
-    # Check if all variables are present in the CSV content
-    CYGNSS_info['status'] = not -1 in start_idx_values
-
-    if CYGNSS_info['status']:
-        # Extract values for each variable
-        for j in range(len(var_list)):
-            if start_idx_keys[j] != 'ddm_timestamp_utc':
-                end_idx = start_idx_values[j + 1] if j < len(var_list) - 1 else len(csv_content)
-                result = re.findall(r'-?\d+(?:\.\d+)?(?:[eE][-+]?\d+)?(?![^\[]*])', csv_content[start_idx_values[j]:end_idx])
-                CYGNSS_info[start_idx_keys[j]] = np.array([float(val) for val in result[offset::4]])
-            else:
-                init_idx = start_idx_values[j] + len(start_idx_keys[j] + ', ')
-                end_idx = csv_content[init_idx:].find('\n') + init_idx
-                CYGNSS_info[start_idx_keys[j]] = np.array([float(val) for val in csv_content[init_idx:end_idx].split(', ')])
+# Function to extract values from NetCDF content
+def extract_values(response, var_list):
+    CYGNSS_info = {}
+    ncfile_path = 'temp.nc4'
+    
+    # Save the NetCDF file locally
+    with open(ncfile_path, 'wb') as f:
+        f.write(response.content)
+    
+    # Read the NetCDF file
+    try:
+        nc_dataset = Dataset(ncfile_path, 'r')
+        for var in var_list:
+            CYGNSS_info[var] = nc_dataset.variables[var][:]
+        # Close the NetCDF dataset
+        nc_dataset.close()
+    except OSError as e:
+        print(f"Error opening NetCDF file: {e}")
+        CYGNSS_info = None
+    
+    # Delete the NetCDF file
+    os.remove(ncfile_path)
+    
     return CYGNSS_info
 
 # Function to make an OpenDAP request for CYGNSS data
@@ -60,15 +94,17 @@ def opendap_request(date, cygID, var_list, retries=3):
     time_res = 1 if (date.year >= 2019 and date.month >= 7) or date.year >= 2020 else 0.5
 
     D, M, YMD = str(date.day).zfill(2), str(date.month).zfill(2), str(date.year) + str(date.month).zfill(2) + str(date.day).zfill(2)
-    url = f"https://opendap.earthdata.nasa.gov/collections/C2146321631-POCLOUD/granules/cyg0{cygID}.ddmi.s{YMD}-000000-e{YMD}-235959.l1.power-brcs.a31.d32.dap.csv?dap4.ce="
+    url = f"https://opendap.earthdata.nasa.gov/collections/C2146321631-POCLOUD/granules/cyg0{cygID}.ddmi.s{YMD}-000000-e{YMD}-235959.l1.power-brcs.a31.d32.dap.nc4?dap4.ce="
 
     for var in var_list:
         url += f"/{var};"
     url = url[:-1]
-
+    
+    username, password = read_credentials('credentials_file')
+    
     for _ in range(retries):
         try:
-            response = requests.get(url, auth=HTTPBasicAuth('username', 'password'))
+            response = requests.get(url, auth=HTTPBasicAuth(username, password))
             response.raise_for_status()  # Raise an HTTPError for bad responses
             return response
         except ChunkedEncodingError as e:
@@ -79,12 +115,12 @@ def opendap_request(date, cygID, var_list, retries=3):
             # Handle other request-related errors if needed
             print(f"Error: {e}")
             break  # Break out of the retry loop for non-chunked encoding errors
-
+    
     # Return None or handle the case where retries are exhausted
     return None
 
 # Load ISMN stations data from JSON file
-file_path = 'H:\\Documents\\CYGNSS\\Data\\ISMN_stations.json'
+file_path = 'ISMN_stations.json'
 with open(file_path, 'r') as json_file:
     stations_dict = json.load(json_file)
 
@@ -100,7 +136,7 @@ end_date = datetime.datetime.strptime(end_date_str, "%Y/%m/%d")
 number_of_days = (end_date - start_date).days
 
 # List of variables to retrieve
-var_list = ['sp_lon', 'sp_lat', 'ddm_snr', 'tx_to_sp_range', 'rx_to_sp_range', 'sp_rx_gain', 'ddm_timestamp_utc']
+var_list = ['sp_lon', 'sp_lat', 'ddm_snr', 'tx_to_sp_range', 'rx_to_sp_range', 'sp_rx_gain', 'ddm_timestamp_utc', 'brcs']
 
 output_data = {'soil_moisture': []}
 
@@ -109,7 +145,7 @@ input_data['date'] = []
 input_data['dist_to_station'] = []
 
 p0 = {} # stations locations
-max_distance = 3  # maximum distance between ISMN sensor and SP location [km]
+max_distance = 5  # maximum distance between ISMN sensor and SP location [km]
 
 cntr = 0
 start_time = time.time()
@@ -118,31 +154,26 @@ time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(start_time - 3*3600))
 print('Process started at ' + time_str)
 
 current_date = start_date
+
 # Loop through each date
 while current_date != end_date:
     for cygID in range(1, 9):
         req = opendap_request(current_date, cygID, var_list)
-
+        
         if req is not None:
             if req.status_code == 200:
-                csv_init = req.content.decode('utf-8')
-
-                for ddmID in range(1, 5):
-                    CYGNSS_info = extract_values(csv_init, var_list, ddmID - 1)
-                    if CYGNSS_info['status']:
+                CYGNSS_info = extract_values(req, var_list)
+                if CYGNSS_info is not None:
+                    for ddmID in range(1, 5):
                         for i in range(num_stations):
                             p0['lat'] = float(stations_dict[str(i)]['Latitude'])
                             p0['lon'] = float(stations_dict[str(i)]['Longitude'])
-                            
-                            dist_to_station = haversine(CYGNSS_info['sp_lat'], CYGNSS_info['sp_lon'], p0['lat'], p0['lon'])
+                            dist_to_station = haversine(CYGNSS_info['sp_lat'][:, ddmID-1], CYGNSS_info['sp_lon'][:, ddmID-1], p0['lat'], p0['lon'])
                             close_points = np.where(dist_to_station < max_distance)[0]
-                            
 
                             if len(close_points) > 0:
-                                print(close_points)
                                 with open(stations_dict[str(i)]['file_path'], 'r') as file:
                                     lines = [line.split() for line in file]
-
                                 dates = np.array([datetime.datetime.strptime(lines[i][0], "%Y/%m/%d") for i in range(1, len(lines))])
                                 secs = np.array([int(lines[i][1][:2]) * 3600 for i in range(1, len(lines))])
 
@@ -150,27 +181,25 @@ while current_date != end_date:
                                     t0 = CYGNSS_info['ddm_timestamp_utc'][point]
                                     idx = np.argmax((dates == current_date) & (np.abs(secs - t0) < 1800))
                                     output_data['soil_moisture'].append(float(lines[idx + 1][2]))
-
-                                update_input_data(input_data, CYGNSS_info, close_points, dist_to_station, var_list, current_date)
-
+                                update_input_data(input_data, CYGNSS_info, close_points, dist_to_station, var_list, current_date, ddmID)
             else:
                 print('404 - CYGNSS Data file not found')
         
         cntr += 1
         elapsed_time = time.time() - start_time
-        remaining_time = elapsed_time / cntr * (number_of_days *8 - cntr)
+        remaining_time = elapsed_time / cntr * (number_of_days * 8 - cntr)
         hours = int(remaining_time // 3600)
         minutes = int((remaining_time % 3600) // 60)
         seconds = int(remaining_time % 60)
 
         num_cases = len(output_data['soil_moisture'])
-        print('Process started at ' + time_str + f' - Number of cases: {num_cases} - Time remaining: {hours} hours, {minutes} minutes, {seconds} seconds')
+        print(f'Number of cases: {num_cases} - Time remaining: {hours} hours, {minutes} minutes, {seconds} seconds')
 
     input_dates = input_data['date']
     input_data['date'] = [str(date) for date in input_data['date']]
     # Save input and output data to JSON files
     with open('input_sm_data.json', 'w') as json_file:
-        json.dump(input_data, json_file, indent=2)
+        json.dump(input_data, json_file, indent=2, default=lambda x: float(x) if isinstance(x, np.float32) else x)
 
     with open('output_sm_data.json', 'w') as json_file:
         json.dump(output_data, json_file, indent=2)
